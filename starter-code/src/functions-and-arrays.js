6b9fe7ab9ce3cac6979d2a7abe6deb5d4ce69d60
Adrian Ajvazi <avashavash@users.noreply.github.com>
2019-09-26 11:57:52 +0200

    finished

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,7 +1,4 @@
-// Find the maximum
-
-// Finding Longest Word
-const words = [
+var words = [
   'mystery',
   'brother',
   'aviator',
@@ -11,16 +8,49 @@ const words = [
   'crackpot'
 ];
 
-// Calculating a Sum
+function longest_string(str_ara) {
+  let max = str_ara[0].length;
+  str_ara.map(v => max = Math.max(max, v.length));
+  result = str_ara.filter(v => v.length == max);
+  return result;
+}
 
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-// Calculate the Average
+console.log(longest_string(words)) // the longest word of the array
+let longestWord = longest_string(words)
+longestWord = longestWord[0].length // how long is the word length
 
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+console.log(longestWord) 
 
-// Array of Strings
-const wordsArr = [
+
+var numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+let sumArray = numbers => {
+    return numbers.reduce(reducer)
+};
+let sumArrayWords = sumArray(numbers)// sum of the words 
+
+console.log(sumArrayWords)
+
+function dividingNumber(sumWords, calcNum) { 
+  return sumWords / calcNum
+}
+
+console.log(dividingNumber(sumArrayWords, longestWord))
+
+var numbersLevelone = [2, 6, 9, 10, 7, 4, 1, 9];
+let sumLevelone  = sumArray(numbersLevelone) 
+let avarageSum = sumLevelone / numbersLevelone.length
+
+console.log(sumLevelone)
+console.log(numbersLevelone.length + " dividing Num")
+console.log(avarageSum)
+
+
+// Write a function averageWordLength that receives an array of words and calculate the average length of the words:
+var wordsLevel2 = [
   'seat',
   'correspond',
   'linen',
@@ -33,8 +63,14 @@ const wordsArr = [
   'palace'
 ];
 
-// Unique Arrays
-const wordsUnique = [
+function averageWordLength (arrayOfWords) {
+ return  sumArray(arrayOfWords).length / arrayOfWords.length
+}
+
+console.log(averageWordLength(wordsLevel2))
+
+
+var duplicateWords = [
   'crab',
   'poison',
   'contagious',
@@ -48,8 +84,19 @@ const wordsUnique = [
   'bring'
 ];
 
-// Finding Elements
-const wordsFind = [
+let uniquifyArray2 = new Set(duplicateWords); // this will also work 
+console.log(uniquifyArray2)
+
+
+let uniquifyArray = (arrayName) => { 
+ return arrayName.filter((item, index) => {
+  return arrayName.indexOf(item) === index
+  })
+}; 
+
+console.log(uniquifyArray(duplicateWords))
+
+var searchWords = [
   'machine',
   'subset',
   'trouble',
@@ -60,8 +107,18 @@ const wordsFind = [
   'disobedience'
 ];
 
-// Counting Repetition
-const wordsCount = [
+let doesWordExist = (wordLooking, arrayNameSearch) => {
+  if(arrayNameSearch.includes(wordLooking)){
+  return true;
+  }else {
+  return false;
+  }
+}
+
+console.log(doesWordExist('bzzz', searchWords))
+
+
+var howManyTimesWords = [
   'machine',
   'matter',
   'subset',
@@ -75,27 +132,18 @@ const wordsCount = [
   'matter'
 ];
 
-// Bonus
 
-const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
-  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
-  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21],
-  [24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
-  [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36],
-  [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16],
-  [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54],
-  [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
-];
+
+
+
+let howManyTimes = (wordArr, timeSearch) => {
+let times = 0;
+for(let i = 0; i < wordArr.length; i++){
+if(wordArr[i] === timeSearch)
+    times++;
+}
+console.log(times)
+};
+
+
+howManyTimes(howManyTimesWords, 'matter')
